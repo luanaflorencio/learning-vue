16770d98735c79d2fa0db77ce721b809fe21caab
luanaflorencio <lua.m.016@gmail.com>
2021-04-29 16:33:44 -0300

    Iniciando mini projeto com vue

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1>ProgramaDEV</h1>
+    <Linguagem descrição="Lorem Ipsum" criador="Guido van Rossum" ano="1989" />
+  </div>
+  
+</template>
+
+<script>
+import Linguagem from './components/Linguagem';
+
+export default {
+  name: 'App',
+  components: {
+    Linguagem
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Linguagem.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <h1>Linguagens de Programação</h1>
+        Qual sua linguagem de programação do seu interesse?<br>
+        <input type="text" placeholder="Ex: Python" id="txt">
+        <input type="submit" value="Procurar" id="btn">
+        <input type="number" v-model="ano">
+        {{ criador }}, {{ ano }}, {{ descrição }}
+    </div>
+</template>
+
+<script>
+export default{
+    data() {
+        return{
+            
+        }
+    },
+    props: {
+        descrição: String,
+        criador: String,
+        ano: Number
+    }
+    
+}
+</script>
+
+<style scoped>
+#btn{
+    background-color: #2760b4;
+    color: white;
+    width: 7%;
+    height: 27px;
+}
+
+#txt{
+    width: 20%;
+    height:30px;
+    margin-right: 1%;
+}
+</style>
